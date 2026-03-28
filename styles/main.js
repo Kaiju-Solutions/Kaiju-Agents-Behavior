cb696003bc32711f7aa79e9d984ae2d58d3dc031
StevenRice99 <StevenRice99@users.noreply.github.com>
2026-03-28 23:19:09 +0000

    deploy: f2d4eb61b7f01508c2bbf2b0359dbc7352936e83

--- styles/main.js
+++ styles/main.js
@@ -3,11 +3,15 @@ function filterAffix() {
   if (!input) return;
   var filter = input.value.toUpperCase();
   var allItems = document.querySelectorAll('#affix ul li');
+  var allUls = document.querySelectorAll('#affix ul');
   
   // Initially show everything if filter is empty
   if (filter === "") {
     for (var i = 0; i < allItems.length; i++) {
       allItems[i].style.display = "";
+    }
+    for (var i = 0; i < allUls.length; i++) {
+      allUls[i].style.display = "";
     }
     return;
   }
@@ -15,6 +19,9 @@ function filterAffix() {
   // Hide everything first
   for (var i = 0; i < allItems.length; i++) {
     allItems[i].style.display = "none";
+  }
+  for (var i = 0; i < allUls.length; i++) {
+    allUls[i].style.display = "none";
   }
 
   // Show matching items and their parents
@@ -25,12 +32,12 @@ function filterAffix() {
       var txtValue = a.textContent || a.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         // Show this item
-        item.style.display = "";
-        // Show all parent li elements to ensure the path to the matching item is visible
+        item.style.display = "block";
+        // Show all parent li and ul elements to ensure the path to the matching item is visible
         var parent = item.parentElement;
         while (parent && parent.id !== 'affix') {
-          if (parent.tagName === 'LI') {
-            parent.style.display = "";
+          if (parent.tagName === 'LI' || parent.tagName === 'UL') {
+            parent.style.display = "block";
           }
           parent = parent.parentElement;
         }
